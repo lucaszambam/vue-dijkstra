507805641097a1816836c1dfa24642e88ee02f92
Lucas Zambam <lucaszambam7896@gmail.com>
2023-09-20 23:28:22 -0300

    first commit

---
+++ vue-dijkstra/src/components/Map.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="map-container">
+        <BrazilSvg />
+    </div>
+</template>
+  
+<script>
+
+import BrazilSvg from './BrazilSvg.vue'
+
+export default {
+    name: 'Map',
+    components: {
+        BrazilSvg
+    },
+}
+</script>
+  
+<style scoped></style>
+  
