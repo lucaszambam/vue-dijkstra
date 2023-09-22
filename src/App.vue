ed440f652c98ec440f69219713fe75296ea79c22
Lucas Zambam <lucaszambam7896@gmail.com>
2023-09-21 23:44:30 -0300

    add github repo area

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <Map />
+  <div class="github-repo">
+    <a href="https://github.com/lucaszambam/vue-dijkstra" target="_blank">
+      <img src="./assets/svg/github-mark-white.svg" alt="GitHub Repository" />
+    </a>
+  </div>
 </template>
 
 <script>
@@ -20,8 +25,23 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
 body {
   background-color: #0d1117;
   height: 99vh;
+}
+.github-repo {
+	position: fixed;
+    bottom: 10px;
+    right: 20px;
+}
+.github-repo a img {
+	scale: 0.45;
+    opacity: 0.5;
+	transition: all .25s;
+}
+.github-repo a img:hover {
+	opacity: 1;
+	scale: 0.5;
 }
 </style>
